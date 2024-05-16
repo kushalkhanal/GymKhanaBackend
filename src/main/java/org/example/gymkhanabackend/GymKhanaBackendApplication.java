a7b14a8ba41810d986bc0133d321e1f089c373b7
kushalkhanal <kushalkhanal181@gmail.com>
2024-05-16 20:07:06 +0545

    backend folder created

---
+++ src/main/java/org/example/gymkhanabackend/GymKhanaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.gymkhanabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymKhanaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymKhanaBackendApplication.class, args);
+	}
+
+}
