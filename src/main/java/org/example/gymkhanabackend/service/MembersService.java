b2f1faa29281cd7e592ea9911c9f9ddecea4130f
AaryanBasnet <basnetaryan1011@gmail.com>
2024-05-20 19:31:48 +0545

    Service created for MembershipTypeService and MembersService

---
+++ src/main/java/org/example/gymkhanabackend/service/MembersService.java
@@ -0,0 +1,15 @@
+package org.example.gymkhanabackend.service;
+
+import org.example.gymkhanabackend.entity.Members;
+import org.example.gymkhanabackend.pojo.MembersPojo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MembersService {
+    void saveData(MembersPojo membersPojo);
+    List<Members> getAll();
+
+    void deleteById(Integer id);
+    Optional<Members> findById(Integer id);
+}
---
+++ src/main/java/org/example/gymkhanabackend/service/MembershipTypeService.java
@@ -0,0 +1,16 @@
+package org.example.gymkhanabackend.service;
+
+import org.example.gymkhanabackend.entity.MembershipType;
+import org.example.gymkhanabackend.pojo.MembershipTypePojo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MembershipTypeService {
+    void saveData(MembershipTypePojo membershipTypePojo);
+    List<MembershipType> getAll();
+
+    void deleteById(Integer id);
+    Optional<MembershipType> findById(Integer id);
+
+}
